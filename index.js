faafc090e62f99dd2b0a8475e6e7e2aabd9e5eb7
Erick Merchant <erickmerchant@gmail.com>
2015-05-24 00:54:22 -0700

    Provide a parse function and use it internally. Stop using run

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ const asyncDone = require('async-done')
 const chalk = require('chalk')
 const unquote = require('unquote')
 const repeat = require('repeat-string')
+const zipObject = require('lodash.zipobject')
 
 class App {
-  constructor (description, argv) {
+  constructor (description, argv, callback) {
+    argv = argv || sergeant.parse(process.argv.slice(2))
+
     this.description = description
-    this.argv = argv
+    this.callback = callback || function (err, result) {
+      if (err) {
+        console.error(chalk.red(err))
+      } else {
+        console.log(result)
+      }
+    }
     this.commands = {}
+    this.argv = {}
+
+    this.argv.args = argv.args || []
+    this.argv.options = argv.options || {}
   }
 
   command (usage, description, options, action) {
-
     const parts = usage.split(' ')
     const name = parts[0]
     const args = parts.slice(1).map(function (v) {
@@ -27,82 +39,34 @@ class App {
       args: args,
       action: action
     }
+
+    if (!this.argv.options.help && this.argv.args.length && this.argv.args[0] === name) {
+      this.argv.args.unshift()
+
+      let context = zipObject(args, this.argv.args.slice(1))
+
+      context.options = this.argv.options
+
+      asyncDone(action.bind(context), this.callback)
+    }
   }
 
-  run (callback) {
+  end () {
+    var error
+    var command
+    var results = []
+    var cols = []
+    var longest = 0
 
-    callback = callback || function () {}
+    if (this.argv.args.length) {
+      if (this.commands[this.argv.args[0]]) {
+        command = this.commands[this.argv.args[0]]
 
-    let context = { options: {} }
-    let command
-
-    try {
-      this.argv.forEach(function (val, key) {
-        if (!command && !val.startsWith('-')) {
-          if (val in this.commands) {
-            command = this.commands[val]
-          } else {
-            throw new Error(val + ' not found')
-          }
-        } else {
-          if (command && !val.startsWith('-')) {
-            for (let i = 0; i < command.args.length; i++) {
-              if (typeof context[command.args[i]] === 'undefined') {
-                context[command.args[i]] = unquote(val)
-
-                break
-              }
-            }
-          } else {
-            if (val.startsWith('--')) {
-              val = val.substr(2)
-
-              if (val) {
-                let parts = val.split('=')
-
-                if (val.startsWith('no-')) {
-                  if (parts.length === 1) {
-                    context.options[parts[0].substr(3)] = false
-                  } else {
-                    throw new Error('negated options don\'t take values')
-                  }
-                } else {
-                  if (parts.length === 1) {
-                    context.options[parts[0]] = true
-                  } else {
-                    context.options[parts[0]] = unquote(parts.slice(1).join('='))
-                  }
-                }
-              }
-            }
-          }
-        }
-      }.bind(this))
-
-      if (context.options.help) {
-        let cols = []
-        let longest = 0
-        if (!command) {
-          console.log(chalk.magenta('Usage:') + ' [options] <command>')
-          console.log(this.description)
-          if (Object.keys(this.commands).length) {
-            console.log(chalk.magenta('Commands:'))
-          }
-
-          for (let c in this.commands) {
-            c = this.commands[c]
-
-            if (c.usage.length > longest) {
-              longest = c.usage.length
-            }
-
-            cols.push([c.usage, c.description])
-          }
-        } else {
-          console.log(chalk.magenta('Usage:') + ' [options] ' + command.usage)
-          console.log(command.description)
+        if (this.argv.options.help) {
+          results.push(chalk.magenta('Usage:') + ' [options] ' + command.usage)
+          results.push(command.description)
           if (Object.keys(command.options).length) {
-            console.log(chalk.magenta('Options:'))
+            results.push(chalk.magenta('Options:'))
           }
 
           for (let o in command.options) {
@@ -113,35 +77,78 @@ class App {
             cols.push([o, command.options[o]])
           }
         }
-
-        longest += 2
-
-        cols.forEach(function (v) {
-          console.log(' ' + chalk.cyan(v[0]) + repeat(' ', longest - v[0].length) + v[1])
-        })
-
-        callback()
       } else {
-        if (command) {
-          asyncDone(command.action.bind(context), function (err, res) {
-            if (err) {
-              console.error(chalk.red(err))
-            }
+        error = new Error(this.argv.args[0] + ' not found')
+      }
+    } else if (this.argv.options.help) {
+      if (!command && !error) {
+        results.push(chalk.magenta('Usage:') + ' [options] <command>')
+        results.push(this.description)
+        if (Object.keys(this.commands).length) {
+          results.push(chalk.magenta('Commands:'))
+        }
 
-            callback(err, res)
-          })
-        } else {
-          throw new Error('run with --help to get a list of commands')
+        for (let c in this.commands) {
+          c = this.commands[c]
+
+          if (c.usage.length > longest) {
+            longest = c.usage.length
+          }
+
+          cols.push([c.usage, c.description])
         }
       }
-    } catch(err) {
-      console.error(chalk.red(err))
-
-      callback(err)
     }
+
+    longest += 2
+
+    cols.forEach(function (v) {
+      results.push(' ' + chalk.cyan(v[0]) + repeat(' ', longest - v[0].length) + v[1])
+    })
+
+    if (!this.argv.args.length && !this.argv.options.help && !error) {
+      error = new Error('run with --help to get a list of commands')
+    }
+
+    this.callback(error, results.join('\n'))
   }
 }
 
-module.exports = function (description, argv) {
-  return new App(description, argv)
+function sergeant (description, argv, callback) {
+  return new App(description, argv, callback)
 }
+
+sergeant.parse = function (argv) {
+  var args = []
+  var options = {}
+
+  argv.forEach(function (val, key) {
+    if (val.startsWith('--')) {
+      val = val.substr(2)
+
+      if (val) {
+        let parts = val.split('=')
+
+        if (val.startsWith('no-')) {
+          if (parts.length === 1) {
+            options[parts[0].substr(3)] = false
+          } else {
+            options[parts[0]] = unquote(parts.slice(1).join('='))
+          }
+        } else {
+          if (parts.length === 1) {
+            options[parts[0]] = true
+          } else {
+            options[parts[0]] = unquote(parts.slice(1).join('='))
+          }
+        }
+      }
+    } else {
+      args.push(unquote(val))
+    }
+  })
+
+  return { args: args, options: options }
+}
+
+module.exports = sergeant
--- test.js
+++ test.js
@@ -3,236 +3,137 @@ var describe = require('mocha').describe
 var it = require('mocha').it
 var sergeant = require('./index.js')
 var chalk = require('chalk')
-var rewire = require('rewire')
 
 describe('module', function () {
   it('should run commands', function (done) {
-    var app = sergeant('a test app', ['test'])
+    var app = sergeant('a test app', { args: ['test'], options: {} }, function (err, result) {
+      assert.ifError(err)
 
-    app.command('test', '', {}, function (d) {
-      assert.ok(true)
+      assert.equal(result, 'ran test')
 
-      d()
+      done()
     })
 
-    app.run(done)
+    app.command('test', '', {}, function (d) {
+      d(null, 'ran test')
+    })
   })
 
   it('accept arguments', function (done) {
-    var app = sergeant('a test app', ['test', '"testing arguments"'])
+    var app = sergeant('a test app', { args: ['test', 'testing arguments'], options: {} }, function (err) {
+      assert.ifError(err)
+
+      done()
+    })
 
     app.command('test <arg>', '', {}, function (d) {
       assert.equal(this.arg, 'testing arguments')
 
       d()
     })
-
-    app.run(done)
   })
 
   it('accept options', function (done) {
-    var app = sergeant('a test app', ['test', '--one="testing"'])
+    var app = sergeant('a test app', { args: ['test'], options: { one: 'testing' } }, function (err) {
+      assert.ifError(err)
+
+      done()
+    })
 
     app.command('test', '', {}, function (d) {
       assert.equal(this.options.one, 'testing')
 
       d()
     })
-
-    app.run(done)
-  })
-
-  it('accept true options', function (done) {
-    var app = sergeant('a test app', ['test', '--one'])
-
-    app.command('test', '', {}, function (d) {
-      assert.equal(this.options.one, true)
-
-      d()
-    })
-
-    app.run(done)
-  })
-
-  it('accept false options', function (done) {
-    var app = sergeant('a test app', ['test', '--no-one'])
-
-    app.command('test', '', {}, function (d) {
-      assert.equal(this.options.one, false)
-
-      d()
-    })
-
-    app.run(done)
   })
 
   it('throws an error when command is not selected', function (done) {
-    var app = sergeant('a test app', [])
-    var errors = []
-    var origError = console.error
-
-    app.command('test', '', {}, function (d) {
-      d()
-    })
-
-    console.error = function (x) {
-      errors.push(x)
-    }
-
-    app.run(function (err) {
+    var app = sergeant('a test app', {}, function (err) {
       assert.equal(err.message, 'run with --help to get a list of commands')
 
-      assert.deepEqual(errors, [chalk.red('Error: run with --help to get a list of commands')])
-
       done()
     })
 
-    console.error = origError
+    app.end()
   })
 
   it('throws an error when command is not selected', function (done) {
-    var app = sergeant('a test app', ['not-defined'])
-    var errors = []
-    var origError = console.error
-
-    app.command('test', '', {}, function (d) {
-      d()
-    })
-
-    console.error = function (x) {
-      errors.push(x)
-    }
-
-    app.run(function (err) {
+    var app = sergeant('a test app', { args: ['not-defined'] }, function (err) {
       assert.equal(err.message, 'not-defined not found')
 
-      assert.deepEqual(errors, [chalk.red('Error: not-defined not found')])
-
       done()
     })
 
-    console.error = origError
-  })
-
-  it('throws an error when you set a value to a negated option', function (done) {
-    var app = sergeant('a test app', ['--no-key=value'])
-    var errors = []
-    var origError = console.error
-
-    app.command('test', '', {}, function (d) {
-      d()
-    })
-
-    console.error = function (x) {
-      errors.push(x)
-    }
-
-    app.run(function (err) {
-      assert.equal(err.message, 'negated options don\'t take values')
-
-      assert.deepEqual(errors, [chalk.red('Error: negated options don\'t take values')])
-
-      done()
-    })
-
-    console.error = origError
+    app.end()
   })
 
   it('provides help for the whole app', function (done) {
-    var sergeant = rewire('./index.js')
-    var lines = []
+    var app = sergeant('a test app', { options: { help: true} }, function (err, result) {
+      assert.ifError(err)
 
-    sergeant.__set__({
-      console: {
-        log: function (x) {
-          lines.push(x)
-        },
-        error: function (err) {
-          throw err
-        }
-      }
-    })
-
-    var app = sergeant('a test app', ['--help'])
-
-    app.command('test <arg>', 'test command', {'--option': 'an option'}, function (d) {
-      d()
-    })
-
-    app.run(function () {
-      assert.deepEqual(lines, [
+      assert.deepEqual(result, [
         chalk.magenta('Usage:') + ' [options] <command>',
         'a test app',
         chalk.magenta('Commands:'),
         ' ' + chalk.cyan('test <arg>') + '  test command'
-      ])
+      ].join('\n'))
 
       done()
     })
+
+    app.command('test <arg>', 'test command', {'--option': 'an option'}, function () { })
+
+    app.end()
   })
 
   it('provides help for each command', function (done) {
-    var sergeant = rewire('./index.js')
-    var lines = []
+    var app = sergeant('a test app', { args: ['test'], options: { help: true } }, function (err, result) {
+      assert.ifError(err)
 
-    sergeant.__set__({
-      console: {
-        log: function (x) {
-          lines.push(x)
-        },
-        error: function () {
-          console.error.apply(undefined, [].slice.call(arguments, 0))
-        }
-      }
+      assert.deepEqual(result, [
+        chalk.magenta('Usage:') + ' [options] test <arg>',
+        'test command',
+        chalk.magenta('Options:'),
+        ' ' + chalk.cyan('--option') + '  an option'
+      ].join('\n'))
+
+      done()
     })
-
-    var app = sergeant('a test app', ['test', '--help'])
 
     app.command('test <arg>', 'test command', {'--option': 'an option'}, function (d) {
       d()
     })
 
-    app.run(function () {
-      assert.deepEqual(lines, [
-        chalk.magenta('Usage:') + ' [options] test <arg>',
-        'test command',
-        chalk.magenta('Options:'),
-        ' ' + chalk.cyan('--option') + '  an option'
-      ])
+    app.end()
+  })
+
+  it('gathers errors from commands', function (done) {
+    var app = sergeant('a test app', { args: ['test'] }, function (err) {
+      assert.equal(err.message, 'nothing bad happened')
 
       done()
     })
-  })
-
-  it('console.errors errors from commands', function (done) {
-    var sergeant = rewire('./index.js')
-    var errs = []
-
-    sergeant.__set__({
-      console: {
-        error: function (x) {
-          errs.push(x)
-        },
-        log: function () {
-          console.log.apply(undefined, [].slice.call(arguments, 0))
-        }
-      }
-    })
-
-    var app = sergeant('a test app', ['test'])
 
     app.command('test <arg>', 'test command', {'--option': 'an option'}, function (d) {
       d(new Error('nothing bad happened'))
     })
+  })
+})
 
-    app.run(function (err) {
-      assert.equal(err.message, 'nothing bad happened')
+describe('module.parse', function () {
+  it('should parse', function (done) {
+    var argv = {
+      args: ['one', 'two'],
+      options: {
+        a: true,
+        b: false,
+        c: 'ccc',
+        'no-d': 'ddd'
+      }
+    }
 
-      assert.deepEqual(errs, [
-        chalk.red('Error: nothing bad happened')
-      ])
+    assert.deepEqual(sergeant.parse(['one', 'two', '--a', '--no-b', '--c=ccc', '--no-d=ddd']), argv)
 
-      done()
-    })
+    done()
   })
 })
