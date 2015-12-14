c48742712f25b0ac1ed8f61c57e26c0fa291b1eb
Erick Merchant <erickmerchant@gmail.com>
2015-12-13 23:40:49 -0800

    Fix main.js

--- code/main.js
+++ code/main.js
@@ -5,7 +5,7 @@ const parse = require('./parse.js')
 const defaultArgv = process.argv.slice(2)
 
 function sergeant (argv) {
-  return new Application(parse(argv || defaultArgv))
+  return new Application({context: parse(argv || defaultArgv)})
 }
 
 sergeant.parse = function (argv) {
