4f2d37bc2acdf8cf7fce278d09bdeec9fd8731b2
Erick Merchant <erickmerchant@gmail.com>
2021-02-05 00:08:13 -0500

    Rework as a simple tool kit

--- main.js
+++ main.js
@@ -1,86 +1,26 @@
-const parse = require('./parse.js')
-const error = require('./error.js')
-const help = require('./help.js')
+import childProcess from 'child_process'
 
-const helpOptions = {
-  help: {
-    description: 'get help'
-  },
-  h: 'help'
-}
+export {default as arg} from 'arg'
 
-const defaultOption = {
-  description: '',
-  multiple: false,
-  required: false,
-  parameter: false,
-  default: null
-}
+export * from 'kleur/colors'
 
-const defaultCommand = {
-  name: '',
-  description: '',
-  signature: [],
-  options: helpOptions,
-  action: null
-}
+export const spawn = (strs, ...quoted) =>
+  new Promise((resolve) => {
+    const args = []
 
-module.exports = (prefix) => {
-  const commands = []
+    for (let i = 0; i < strs.length; i++) {
+      args.push(...strs[i].split(' '))
 
-  return {
-    command(definition) {
-      definition = {...defaultCommand, ...definition}
-
-      definition.options = {...definition.options, ...helpOptions}
-
-      for (const [key, option] of Object.entries(definition.options)) {
-        if (typeof option !== 'object') continue
-
-        definition.options[key] = {...defaultOption, ...option}
-      }
-
-      commands.unshift(definition)
-    },
-    async start(argv) {
-      let command
-
-      for (const definition of commands) {
-        if (definition.name === argv[0] || definition.name === '') {
-          command = definition
-
-          if (command.name !== '') {
-            break
-          }
-        }
-      }
-
-      if (command == null) {
-        command = defaultCommand
-      }
-
-      const subCommands = command.name === '' ? commands : []
-
-      try {
-        const args = parse(command.name === '' ? argv : argv.slice(1), {
-          signature: command.signature,
-          options: command.options
-        })
-
-        if (args == null || args.help || command.action == null) {
-          help(prefix, {
-            commands: subCommands,
-            name: command.name,
-            description: command.description,
-            signature: command.signature,
-            options: command.options
-          })
-        } else if (command.action != null) {
-          await command.action(args)
-        }
-      } catch (e) {
-        error(e)
+      if (quoted[i] != null) {
+        args.push(quoted[i])
       }
     }
-  }
-}
+
+    const spawned = childProcess.spawn(args[0], args.slice(1), {
+      stdio: 'inherit'
+    })
+
+    spawned.on('exit', () => {
+      resolve()
+    })
+  })
